9340b6afd7bdebd415c3f6d88c5d0a30159ee94c
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-03-30 18:28:43 +0400

    sql task

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- Something useful
+CREATE database if not exists hbnb_dev_db;
+CREATE user if not exists 'hbnb_dev'@'localhost' identified by 'hbnb_dev_pwd';
+GRANT all privileges on hbnb_dev_db.* to 'hbnb_dev'@'localhost';
+GRANT SELECT on performance_schema.* to 'hbnb_dev'@'localhost';
