20a814e61f9f6645f2aa2f3a13db959a651a00ff
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-03-30 18:07:38 +0400

    base model fixes

--- models/base_model.py
+++ models/base_model.py
@@ -8,18 +8,14 @@ class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    kwargs[key] = datetime.strptime(kwargs['updated_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
             self.__dict__.update(kwargs)
 
     def __str__(self):
