d527851b79d4cab2b194631c898e8c338fbc9f3b
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-03-31 15:10:51 +0400

    baxaqda

--- models/city.py
+++ models/city.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchmey import Column, String, ForeignKey
-from sqlalchmey.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
