996afb6c96d0a0e7ff9a290048fc7c2b758fa65a
dArk10R4 <goylerov04@gmail.com>
2024-04-03 23:09:47 +0400

    7

--- models/user.py
+++ models/user.py
@@ -6,7 +6,9 @@ from models.base_model import BaseModel, Base
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = 'users'
     email = ''
     password = ''
     first_name = ''
     last_name = ''
+    
